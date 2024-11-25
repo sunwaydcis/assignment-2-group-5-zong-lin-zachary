45647c3e5c16858d4e5f35b0323e84c127d40c07
Goo Zong Lin <zonglingoo@gmail.com>
2024-11-25 23:04:34 +0800

    Load csv file with validation check

---
+++ src/main/scala/myfirstscala.scala
@@ -0,0 +1,24 @@
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scala.io.Source
+import scala.collection.mutable.mutable.Map
+
+// Load Data with validation 
+val data = try {
+  val loadedData = readHospitalData("hosiptal.csv")
+  println("Data loaded successfully.")
+  loadedData
+} catch {
+  case e: java.io.FileInputStreamException =>
+    println("File not found.")
+    System.exit(1)
+    List.empty
+}
+
+object MyApp extends JFXApp3:
+
+  override def start(): Unit =
+    stage = new PrimaryStage()
+
+      // Set the title of the window
+end MyApp
